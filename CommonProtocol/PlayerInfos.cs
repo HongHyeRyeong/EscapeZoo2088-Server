3797d2553f93bb8785778dbd12067e6977eec6af
shgpwn <shgpwn1203@gmail.com>
2022-10-10 20:23:30 +0900

    [fix] LastGame - PlayerInfos

---
+++ CommonProtocol/PlayerInfos.cs
@@ -0,0 +1,16 @@
+﻿namespace CommonProtocol
+{
+    public class PlayerInfos
+    {
+        public string userId;
+        public string mbti;
+        public int animal;
+
+        public PlayerInfos(string userId, string mbti, int animal)
+        {
+            this.userId = userId;
+            this.mbti = mbti;
+            this.animal = animal;
+        }
+    }
+}
