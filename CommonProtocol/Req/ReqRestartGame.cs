cc0846024b5c2382bcf4a11a9dd776fd00036a32
shgpwn <shgpwn1203@gmail.com>
2022-10-10 20:22:30 +0900

    [fix] RestartGame

---
+++ CommonProtocol/Req/ReqRestartGame.cs
@@ -0,0 +1,15 @@
+﻿namespace CommonProtocol
+{
+    public class ReqRestartGame : CBaseProtocol
+    {
+        public string gameSessionId;
+        public string teamName;
+        public string playerSessionId;
+        public string userId;
+        public string mbti;
+        public int animal;
+        public int currentRoundNum;
+        public int endRoundNum;
+        public int teamUserCount;
+    }
+}
