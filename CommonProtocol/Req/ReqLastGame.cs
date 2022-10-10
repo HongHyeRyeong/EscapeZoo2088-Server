ae398fcbf4f0f9af86178be7fe004d3871b2aac3
shgpwn <shgpwn1203@gmail.com>
2022-10-10 20:22:48 +0900

    [fix] LastGame

---
+++ CommonProtocol/Req/ReqLastGame.cs
@@ -0,0 +1,15 @@
+﻿namespace CommonProtocol
+{
+    public class ReqLastGame : CBaseProtocol
+    {
+        public string gameSessionId;
+        public string teamName;
+        public string playerSessionId;
+        public string userId;
+        public string mbti;
+        public int animal;
+        public int currentRoundNum;
+        public int endRoundNum;
+        public int teamUserCount;
+    }
+}
---
+++ CommonProtocol/Res/ResLastGame.cs
@@ -0,0 +1,12 @@
+﻿namespace CommonProtocol
+{
+    public class ResLastGame : CBaseProtocol
+    {
+        public ResponseType ResponseType;
+        public string gameSessionId;
+        public string teamName;
+        public string userId;
+        public int enemyRoundNum;
+        public bool isWinner;
+    }
+}
