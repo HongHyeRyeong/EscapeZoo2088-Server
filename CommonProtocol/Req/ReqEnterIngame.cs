86b56b8792ce78a7ebadd34c392a36c89bd03ea5
shgpwn <shgpwn1203@gmail.com>
2022-10-10 20:22:58 +0900

    [fix] EnterIngame

---
+++ CommonProtocol/Req/ReqEnterIngame.cs
@@ -0,0 +1,9 @@
+﻿namespace CommonProtocol
+{
+    public class ReqEnterIngame : CBaseProtocol
+    {
+        public string gameSessionId;
+        public string teamName;
+        public int teamUserCount;
+    }
+}
---
+++ CommonProtocol/Res/ResEnterIngame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CommonProtocol
+{
+    public class ResEnterIngame : CBaseProtocol
+    {
+        public ResponseType ResponseType;
+        public string gameSessionId;
+        public string teamName;
+        public List<PlayerInfos> playerInfos;
+    }
+}
