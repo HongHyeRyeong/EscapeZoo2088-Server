748c9a582855e342055cfe5eee4d8e99b449551c
shgpwn <shgpwn1203@gmail.com>
2022-10-10 20:22:18 +0900

    [fix] StartGame

---
+++ CommonProtocol/Res/ResStartGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace CommonProtocol
+{
+    public class ResStartGame : CBaseProtocol
+    {
+        public ResponseType ResponseType;
+        public string gameSessionId;
+        public string teamName;
+        public string playerSessionId;
+        public string userId;
+        public int currentRoundNum;
+        public int endRoundNum;
+        public List<int> roundList;
+        public int enemyRoundNum;
+        public long sunriseTime;
+    }
+}
