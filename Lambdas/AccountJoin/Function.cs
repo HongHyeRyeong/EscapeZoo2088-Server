5b924a6abc814db7c407746e853a6101f87de7c3
sunghyun <sunghyun.kim3@nhn.com>
2022-10-12 01:29:47 +0900

    [feat] matchStatus , AccountJoin

--- Lambdas/AccountJoin/Function.cs
+++ Lambdas/AccountJoin/Function.cs
@@ -38,7 +38,13 @@ namespace AccountJoin
                 query.Clear();
                 query.Append("INSERT INTO users (userid, password,mbti, win, loss, score) VALUES ('")
                     .Append(req.userId).Append("','").Append(req.password).Append("','").Append(req.mbti).Append("',0, 0, 1000);");
+
+                query.Clear();
+                query.Append("INSERT INTO gameInfo (gameSessionId, teamName,userid, mbti,roundList) VALUES ('")
+                     .Append(req.userId).Append("','").Append(req.userId).Append("','").Append(req.userId).Append("','").Append(req.mbti).Append("','');");
+
                 await db.ExecuteNonQueryAsync(query.ToString());
+
                 res.userId = db.LastInsertedId().ToString();
             }
             return res;
--- Lambdas/MatchStatus/Function.cs
+++ Lambdas/MatchStatus/Function.cs
@@ -1,11 +1,13 @@
-using System.Threading.Tasks;
+ï»¿using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using CommonProtocol;
 using Amazon.GameLift;
 using Amazon.GameLift.Model;
-using GameLiftWrapper;
+using System.Collections.Generic;
 using System;
-
+using System.Linq;
+using GameDB;
+using System.Text;
 
 [assembly: LambdaSerializer(typeof(CustomSerializer.LambdaSerializer))]
 
@@ -16,16 +18,12 @@ namespace MatchStatus
         public async Task<ResMatchStatus> FunctionHandler(ReqMatchStatus req, ILambdaContext context)
         {
 
-
             var res = new ResMatchStatus
             {
                 ResponseType = ResponseType.Success
             };
 
             var client = new AmazonGameLiftClient();
-            var clientInfo = GameInfo.Instance();
-
-
 
             var match_response = await client.DescribeMatchmakingAsync(new DescribeMatchmakingRequest
             {
@@ -40,7 +38,29 @@ namespace MatchStatus
                 int Port = ticketInfo.GameSessionConnectionInfo.Port;
                 string TeamName = ticketInfo.Players[0].Team;
                 string Gamesessionid = ticketInfo.GameSessionConnectionInfo.GameSessionArn;
-                
+
+                //Random randomObj = new Random();
+                //List<int> roundList = new List<int>();
+                //for (int i = 0; i < 7; i++)
+                //{
+                //    roundList.Add(i);
+                //}
+
+                //int random1, temp;
+                //for (int i = 0; i < roundList.Count; ++i)
+                //{
+                //    random1 = randomObj.Next(0, roundList.Count - 1);
+
+                //    temp = roundList[i];
+                //    roundList[i] = roundList[random1];
+                //    roundList[random1] = temp;
+                //}
+
+                //List<string> strRoundList = roundList.Select(i => i.ToString()).ToList();
+                //string strRound = string.Join("|", strRoundList); 
+
+                //long sunriseTime = randomObj.Next(10, 25);
+                //string strSunriseTime = sunriseTime.ToString();
 
                 foreach (MatchedPlayerSession psess in ticketInfo.GameSessionConnectionInfo.MatchedPlayerSessions)
                 {
@@ -52,12 +72,22 @@ namespace MatchStatus
                         res.Port = Port + 1;
                     res.TeamName = TeamName;
                     res.Gamesessionid = Gamesessionid;
-
-                    //gameInfo¿¡ °ª ³Ö¾îÁÜ
-                    clientInfo.addUser(Gamesessionid, TeamName, req.userId,req.mbti,req.character, -1);
-                    res.roundList = clientInfo.getRoundList(Gamesessionid, TeamName);
+                    List<int> tempList = new List<int> { 0, 1, 3, 4, 2 };
+                    res.roundList = tempList;
                     break;
                 }
+                //DBEnv.SetUp();
+                //using (var db = new DBConnector())
+                //    {
+                //        var query = new StringBuilder();
+                //        query.Append("update gameInfo set gameSessionId = '")
+                //        .Append(Gamesessionid).Append("',teamName ='").Append(TeamName).Append("',animal ='").Append(req.character)
+                //        .Append("',roundNum = -1").Append(",roundList ='").Append(strRound)
+                //        .Append("',sunriseTime ='").Append(strSunriseTime)
+                //        .Append("' where userid = '").Append(req.userId).Append("';");
+                //        await db.ExecuteNonQueryAsync(query.ToString());
+                //    }
+
             }
             else
             {
