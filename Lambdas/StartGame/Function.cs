60d25e61d61a70da60b6bbc9a1e24a8c3b02340d
shgpwn <shgpwn1203@gmail.com>
2022-10-14 01:12:22 +0900

    [fix] StartGame

--- Lambdas/StartGame/Function.cs
+++ Lambdas/StartGame/Function.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using CommonProtocol;
 using GameLiftWrapper;
+using GameDB;
+using System.Text;
+using System.Data;
 
 [assembly: LambdaSerializer(typeof(CustomSerializer.LambdaSerializer))]
 
@@ -11,6 +14,11 @@ namespace StartGame
 {
     public class Function
     {
+        public Function()
+        {
+            DBEnv.SetUp();
+        }
+
         public async Task<ResStartGame> FunctionHandler(ReqStartGame req, ILambdaContext context)
         {
             var res = new ResStartGame
@@ -18,92 +26,171 @@ namespace StartGame
                 ResponseType = ResponseType.Fail
             };
 
-            GameInfo gameInfo = GameInfo.Instance();
+            //GameInfo gameInfo = GameInfo.Instance();
             int currentRoundNum = req.preRoundNum + 1;
-            gameInfo.addUser(req.gameSessionId, req.teamName, req.userId, req.mbti, req.animal, currentRoundNum);
-
-            DateTime currentDateTime = DateTime.UtcNow;
-            int totalUserCount = req.teamUserCount;
-            if (req.preRoundNum == 0)
+            //gameInfo.addUser(req.gameSessionId, req.teamName, req.userId, req.mbti, req.animal, currentRoundNum);
+            bool isSuccessRoundNum = false;
+            using (var db = new DBConnector())
             {
-                totalUserCount *= 2;
-            }
+                var query = new StringBuilder();
+                query.Append("UPDATE gameInfo SET roundNum = ")
+                    .Append(currentRoundNum)
+                    .Append(" WHERE userid = '")
+                    .Append(req.userId).Append("';");
+                await db.ExecuteNonQueryAsync(query.ToString());
+                isSuccessRoundNum = true;
 
-            int checkUserCount = 0;
-            while (checkUserCount < totalUserCount)
-            {
-                DateTime checkDateTime = DateTime.UtcNow;
-                TimeSpan dateDiff = checkDateTime - currentDateTime;
-                int diffSecond = dateDiff.Seconds;
-                if (diffSecond < 1)
+                if (!isSuccessRoundNum)
                 {
-                    continue;
+                    Console.WriteLine("Fail update RoundNum");
+                    return res;
                 }
-                currentDateTime = checkDateTime;
 
+                DateTime currentDateTime = DateTime.UtcNow;
+                int totalUserCount = req.teamUserCount;
                 if (req.preRoundNum == 0)
                 {
-                    checkUserCount = gameInfo.getUserCountInGame(req.gameSessionId);
+                    totalUserCount *= 2;
                 }
-                else
+
+                int checkUserCount = 0;
+                while (checkUserCount < totalUserCount)
                 {
-                    checkUserCount = gameInfo.getUserCountInRound(req.gameSessionId, req.teamName, currentRoundNum);
+                    DateTime checkDateTime = DateTime.UtcNow;
+                    TimeSpan dateDiff = checkDateTime - currentDateTime;
+                    int diffSecond = dateDiff.Seconds;
+                    if (diffSecond < 1)
+                    {
+                        continue;
+                    }
+                    currentDateTime = checkDateTime;
+
+                    if (req.preRoundNum == 0)
+                    {
+                        query.Clear();
+                        query.Append("SELECT userid FROM gameInfo where gameSessionId = '")
+                            .Append(req.gameSessionId).Append("';");
+
+                        using (var cursor = await db.ExecuteReaderAsync(query.ToString()))
+                        {
+                            if (cursor.Read())
+                            {
+                                checkUserCount++;
+                            }
+                        }
+                        await db.ExecuteNonQueryAsync(query.ToString());
+                    }
+                    else
+                    {
+                        //checkUserCount = gameInfo.getUserCountInRound(req.gameSessionId, req.teamName, currentRoundNum);
+                        query.Clear();
+                        query.Append("SELECT * FROM gameInfo where gameSessionId = '")
+                           .Append(req.gameSessionId).Append("' AND teamName = '")
+                           .Append(req.teamName).Append("' AND roundNum = ")
+                           .Append(currentRoundNum).Append(";");
+                        using (var cursor = await db.ExecuteReaderAsync(query.ToString()))
+                        {
+                            if (cursor.Read())
+                            {
+                                checkUserCount++;
+                            }
+                        }
+                        await db.ExecuteNonQueryAsync(query.ToString());
+                    }
+
+                    if (checkUserCount == 0)
+                    {
+                        break;
+                    }
                 }
 
                 if (checkUserCount == 0)
                 {
-                    break;
+                    Console.WriteLine("checkUserCount is Zero");
+                    return res;
                 }
-            } 
-            if (checkUserCount == 0)
-            {
-                Console.WriteLine("checkUserCount is Zero");
-                return res; 
-            }
 
-            Console.WriteLine("Success");
+                Console.WriteLine("Success");
 
 
-            res.ResponseType = ResponseType.Success;
-            res.gameSessionId = req.gameSessionId;
-            res.teamName = req.teamName;
-            res.playerSessionId = req.playerSessionId;
-            res.userId = req.userId;
-            res.currentRoundNum = currentRoundNum;
-            res.endRoundNum = req.endRoundNum;
+                res.ResponseType = ResponseType.Success;
+                res.gameSessionId = req.gameSessionId;
+                res.teamName = req.teamName;
+                res.playerSessionId = req.playerSessionId;
+                res.userId = req.userId;
+                res.currentRoundNum = currentRoundNum;
+                res.endRoundNum = req.endRoundNum;
+
+                //List<int> roundList = gameInfo.getRoundList(req.gameSessionId, req.teamName);
+                res.roundList = new List<int>();
+                string roundListInfo = "";
+                query.Clear();
+                query.Append("SELECT * FROM gameInfo where gameSessionId = '")
+                    .Append(req.gameSessionId).Append("' AND teamName = '")
+                    .Append(req.teamName).Append("';");
+                
+                using (var cursor = await db.ExecuteReaderAsync(query.ToString()))
+                {
+                    if (cursor.Read())
+                    {
+                        roundListInfo = cursor["roundList"].ToString();
+                        res.sunriseTime = Convert.ToInt64(cursor["sunriseTime"].ToString());
+                    }
+                }
+                await db.ExecuteNonQueryAsync(query.ToString());
             
-            List<int> roundList = gameInfo.getRoundList(req.gameSessionId, req.teamName);
-            res.roundList = new List<int>();
-            string roundListInfo = "";
-            foreach (int num in roundList)
-            {
-                res.roundList.Add(num);
-                roundListInfo += num + " | ";
+                string[] roundStr = roundListInfo.Split('|');
+                foreach (var round in roundStr)
+                {
+                    res.roundList.Add(Convert.ToInt32(round));
+                }
+                //foreach (int num in roundList)
+                //{
+                //    res.roundList.Add(num);
+                //    roundListInfo += num + " | ";
+                //}
+
+                int enemyRoundMax = 0;
+                //Dictionary<string, int> enemyRoundDict = gameInfo.getEnemyRound(req.gameSessionId, req.teamName);
+                //foreach (KeyValuePair<string, int> keyValuePair in enemyRoundDict)
+                //{
+                //    enemyRoundValue = keyValuePair.Value;
+                //    break;
+                //}
+                query.Clear();
+                query.Append("SELECT * FROM gameInfo where gameSessionId = '")
+                    .Append(req.gameSessionId).Append("' AND teamName != '")
+                    .Append(req.teamName).Append("';");
+                
+                using (var cursor = await db.ExecuteReaderAsync(query.ToString()))
+                {
+                    if (cursor.Read())
+                    {
+                        int result = Convert.ToInt32(cursor["roundNum"].ToString());
+                        if (enemyRoundMax < result)
+                        {
+                            enemyRoundMax = result;
+                        }
+                    }
+                }
+                await db.ExecuteNonQueryAsync(query.ToString());
+                res.enemyRoundNum = enemyRoundMax;
+
+                //res.sunriseTime = gameInfo.getSunriseTime(req.gameSessionId, req.teamName);
+
+                Console.WriteLine("Success");
+                Console.WriteLine("ResponseType:" + ResponseType.Success);
+                Console.WriteLine("gameSessionId:" + req.gameSessionId);
+                Console.WriteLine("teamName:" + req.teamName);
+                Console.WriteLine("playerSessionId:" + req.playerSessionId);
+                Console.WriteLine("userId:" + req.userId);
+                Console.WriteLine("currentRoundNum:" + currentRoundNum);
+                Console.WriteLine("endRoundNum:" + req.endRoundNum);
+                Console.WriteLine("roundList:" + roundListInfo);
+                Console.WriteLine("enemyRoundNum:" + enemyRoundMax);
+                Console.WriteLine("sunriseTime:" + res.sunriseTime);
+                return res;
             }
-
-            int enemyRoundValue = 0;
-            Dictionary<string, int> enemyRoundDict = gameInfo.getEnemyRound(req.gameSessionId, req.teamName);
-            foreach (KeyValuePair<string, int> keyValuePair in enemyRoundDict)
-            {
-                enemyRoundValue = keyValuePair.Value;
-                break;
-            }
-            res.enemyRoundNum = enemyRoundValue;
-
-            res.sunriseTime = gameInfo.getSunriseTime(req.gameSessionId, req.teamName);
-
-            Console.WriteLine("Success");
-            Console.WriteLine("ResponseType:" + ResponseType.Success);
-            Console.WriteLine("gameSessionId:" + req.gameSessionId);
-            Console.WriteLine("teamName:" + req.teamName);
-            Console.WriteLine("playerSessionId:" + req.playerSessionId);
-            Console.WriteLine("userId:" + req.userId);
-            Console.WriteLine("currentRoundNum:" + currentRoundNum);
-            Console.WriteLine("endRoundNum:" + req.endRoundNum);
-            Console.WriteLine("roundList:" + roundListInfo);
-            Console.WriteLine("enemyRoundNum:" + enemyRoundValue);
-            Console.WriteLine("sunriseTime:" + res.sunriseTime);
-            return res;
         }
     }
 }
